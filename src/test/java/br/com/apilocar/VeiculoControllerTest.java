c55d4a0913a9ca80696ebfa411718ad6e159d4c3
Ailton2 <tym.ailton@hotmail.com>
2021-10-22 08:43:51 -0300

    subindo apilocar

--- src/test/java/br/com/apilocar/VeiculoControllerTest.java
+++ src/test/java/br/com/apilocar/VeiculoControllerTest.java
@@ -1,19 +1,20 @@
-package br.com.apirest.controller;
+package br.com.apilocar;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
 import br.com.apilocar.controller.VeiculoController;
 import io.restassured.http.ContentType;
 
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
-
-@WebMvcTest
+@SpringBootTest
 public class VeiculoControllerTest {
-	
 
 	@Autowired
 	private VeiculoController veiculoController;
